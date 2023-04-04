ef504f8df6e8daf5e2f5631102f9b23e61ce452d
Vladyslav Parfinenko <ronwaylla@gmail.com>
2023-04-04 19:05:58 +0300

    дз 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const btnEl = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+
+btnEl.addEventListener('click', changeColor);
+
+function changeColor(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = getRandomHexColor();
+
+}
