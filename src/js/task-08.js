d395e58619e7855b737db8a08cd586531b442b78
Vladyslav Parfinenko <ronwaylla@gmail.com>
2023-04-04 18:54:08 +0300

    Дз 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,28 @@
+const form = document.querySelector('.login-form');
 
+
+form.addEventListener('submit', checkUser);
+
+
+function checkUser(event) {
+    event.preventDefault();
+
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (!email || !password) {
+        alert('Заполните все поля');
+        return;
+    }
+
+    const userData = {
+        email,
+        password,
+    };
+
+    console.log(userData);
+    form.reset();
+    
+        
+        
+}
